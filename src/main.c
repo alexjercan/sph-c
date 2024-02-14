61cbea8af3d021001eaf2296149a1bb834543639
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-14 06:53:17 +0200

    start

--- src/main.c
+++ src/main.c
@@ -10,35 +10,34 @@
 #define PARTICLE_COUNT 1024 * 2
 #define PARTICLE_RADIUS 2.0f
 
-struct particle {
-        Vector2 position;
-        Vector2 velocity;
-};
-
 int main() {
     SetRandomSeed(time(NULL));
 
-    struct particle particles[PARTICLE_COUNT];
+    struct Vector2 particle_positions[PARTICLE_COUNT];
 
     for (int i = 0; i < PARTICLE_COUNT; i++) {
-        particles[i].position = (struct Vector2){
+        particle_positions[i] = (struct Vector2){
             GetRandomFloat(0, SCREEN_WIDTH), GetRandomFloat(0, SCREEN_HEIGHT)};
-        particles[i].velocity = (struct Vector2){0.0f, 0.0f};
     }
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "water simulation");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Smoothed Particle Hydrodynamics");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         if (IsKeyDown(KEY_R)) {
+            for (int i = 0; i < PARTICLE_COUNT; i++) {
+                particle_positions[i] =
+                    (struct Vector2){GetRandomFloat(0, SCREEN_WIDTH),
+                                     GetRandomFloat(0, SCREEN_HEIGHT)};
+            }
         }
 
         BeginDrawing();
         ClearBackground(DARKGRAY);
 
         for (int i = 0; i < PARTICLE_COUNT; i++) {
-            DrawCircleV(particles[i].position, PARTICLE_RADIUS, BLUE);
+            DrawCircleV(particle_positions[i], PARTICLE_RADIUS, BLUE);
         }
 
         EndDrawing();
