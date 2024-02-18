16b649d975e9a04b537284d2ccdd607e8cbd2610
Alex Jercan <jercan_alex27@yahoo.com>
2024-02-18 19:55:19 +0200

    ci: why not

--- web/build.sh
+++ web/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p dist/wasm
-clang --target=wasm32 -I./include -I../build/_deps/raylib-src/src/ -I../src \
+clang --target=wasm32 -I./include -I../src \
     --no-standard-libraries -Wl,--export-table -Wl,--no-entry \
     -Wl,--allow-undefined -Wl,--export=main -o dist/wasm/particle_simulator.wasm \
     particle_simulator.c ../src/*.c -DSPH_NO_STDIO
